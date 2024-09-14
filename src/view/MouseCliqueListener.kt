9542aed1c125822ba128c55d74b2179634225fb8
Wellington Damasceno <wellfurtado@gmail.com>
2024-09-13 21:41:28 -0300

    Criação do Tabuleiro e evento de clique do mouse em MouseCliqueListener.kt

---
+++ src/view/MouseCliqueListener.kt
@@ -0,0 +1,19 @@
+package view
+
+import model.Campo
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+
+class MouseCliqueListener(private val campo: Campo, private val onBotaoEsquerdo: (Campo) -> Unit, private val onBotaoDireito: (Campo) -> Unit) : MouseListener{
+    override fun mousePressed(e: MouseEvent?) {
+        when (e?.button) {
+            MouseEvent.BUTTON1 -> onBotaoEsquerdo(campo)
+            MouseEvent.BUTTON2 -> onBotaoDireito(campo)
+        }
+    }
+
+    override fun mouseClicked(e: MouseEvent?) {}
+    override fun mouseExited(e: MouseEvent?) {}
+    override fun mouseEntered(e: MouseEvent?) {}
+    override fun mouseReleased(e: MouseEvent?) {}
+}
